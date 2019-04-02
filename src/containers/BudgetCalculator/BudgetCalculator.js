ed64ee874598aa2a3bc3a3c3d05bfaede1bb1f65
kajamiko <kajaths@gmail.com>
2019-04-02 15:15:56 +0100

    Implemented method of passing form to parent element

--- src/containers/BudgetCalculator/BudgetCalculator.js
+++ src/containers/BudgetCalculator/BudgetCalculator.js
@@ -5,38 +5,54 @@ import RecordForm from '../RecordForm/RecordForm';
 
 class BudgetCalculator extends Component {
 
-    state = {
-        income: [
-            {
-                description: "some desc",
-                value: "1000"
-            },
-            {
-                description: "Salary",
-                value: "1800" 
-            }
-        ],
-        expenses: [
-            {
-                description: "some expense",
-                value: "500",
-                percentage: -1
-            }
-        ],
-        totalBudget: 0,
-        percentage: 0
-    }
-
-    onRecordChange = (name, value) => {
+    constructor(props) {
+        super(props);
+        this.state = {
+            income: [
+                {
+                    description: "Salary",
+                    value: "1800" 
+                }
+            ],
+            expenses: [
+                {
+                    description: "some expense",
+                    value: "500",
+                    percentage: -1
+                }
+            ],
+            totalBudget: 0,
+            percentage: 0
+        }
+        this.handleClick = this.handleClick.bind(this);
 
     }
+    
+
+    handleClick(event) {
+        // creating new object needed, after validation passing to parent state
+        let newObject = {
+             type: nevent.target.type.value,
+                         description: event.target.description.value,
+                          value:  event.target.description.value}
+        if (type && description && value) {
+            console.log("worked");
+            if (type === "income") {
+
+            }
+        }
+        // console.log(event.target.value.value);
+        // console.log(event.target.description.value);
+        // console.log(event.target.description.value);
+        event.preventDefault();
+      };
 
 render() {
     let income = <RecordsList type="income" dataset={this.state.income}></RecordsList>;
     let expenseList = <RecordsList type="expenses" dataset={this.state.expenses}></RecordsList>;
     return (
         <div>
-            <RecordForm />
+            <RecordForm handleClick={this.handleClick.bind(this)} />
             <div className={classes.ListsContainer}>
                 {income}
                 {expenseList}
--- src/containers/RecordForm/RecordForm.js
+++ src/containers/RecordForm/RecordForm.js
@@ -3,39 +3,6 @@ import classes from './RecordForm.module.css';
 
 class RecordForm extends Component {
   
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//         type: null,
-//        description: null,
-//        value: null
-//       };
-
-//     this.changeHandler = this.changeHandler .bind(this);
-//   }
-
-//   changeHandler (event) {
-//     const target = event.target;
-//     const value = target.type === 'checkbox' ? target.checked : target.value;
-//     const name = target.name;
-
-//     console.log(event.target.name);
-//     this.setState({
-//       [name]: value
-//     });
-//   }
-  
-   
-//     render() {
-//         return (
-//             <div>
-//             <BudgetRecordInput name="type" type="type" onChange={this.changeHandler}/>
-//             <BudgetRecordInput name="description" type="description" onChange={this.changeHandler}/>
-//             <BudgetRecordInput name="value" type="value" onChange={this.changeHandler}/>
-//             </div>
-//         );
-//     }
-
 constructor(props) {
     super(props);
 this.state = {
@@ -47,36 +14,43 @@ this.state = {
     this.handleInputChange = this.handleInputChange.bind(this);
   }
 
+//   handleClick(event) {
+      
+//     console.log("Worked");
+//     event.preventDefault();
+//   };
+
+
   handleInputChange(event) {
     const target = event.target;
     const value = target.value;
     const name = target.name;
-    console.log(event.target.name);
     this.setState({
       [name]: value
     });
+
   }
   render() {
     return (
-      <form>
-        <select name="type" defaultValue="income" className={classes.FormType} onChange={this.handleInputChange}>
+      <form onSubmit={this.props.handleClick} className={classes.Form}>
+        <select name="type" defaultValue="income" className={`${classes.Input} ${classes.FormType}`} onChange={this.handleInputChange}>
                      <option value="income" >+</option>
                      <option value="expenses">-</option>
                  </select>
-            <input 
+            <input className={`${classes.Input} ${classes.Element} ${classes.TextOnes}`}
                 name="description" 
-                className={classes.FormInput} 
                 type="text" 
                 placeholder="Description" 
                 onChange={this.handleInputChange}/>
 
-          <input
+          <input className={`${classes.Input} ${classes.Element} ${classes.TextOnes}`}
             name="value"
             type="number"
-            // value={this.state.numberOfGuests}
             placeholder="Value"
             onChange={this.handleInputChange} />
-
+        <input className={`${classes.Input} ${classes.Submit}`}
+        type="submit" 
+        value="Add"/>
       </form>
     );
   }
